d6e943961d975c74ae6ba788a6febd97f36a35a2
Ziaullah22 <ziaullah4127@gmail.com>
2026-04-06 08:44:08 -0700

    fixes

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App';
+import App from './App.tsx';
 import './index.css';
-import { ThemeProvider } from './context/ThemeContext';
-import { AuthProvider } from './context/AuthContext';
+import { ThemeProvider } from './context/ThemeContext.tsx';
+import { AuthProvider } from './context/AuthContext.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
