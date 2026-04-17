6f7d53fb0eaa6ddf8705358d0fd7bca25eaf9b14
Ziaullah22 <ziaullah4127@gmail.com>
2026-04-16 18:11:42 -0700

    fixes

--- admin/vite.config.ts
+++ admin/vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/admin-portal/',
   plugins: [react()],
   server: {
     port: 5174,
